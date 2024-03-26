bd2ac578b56f112d4bbe2d3776e28e6cc547d8b5
Autostrafe <Levibautista360@gmail.com>
2024-03-26 12:06:05 -0600

    A7 String Basics Finished

--- A7 String Basics/src/App.java
+++ A7 String Basics/src/App.java
@@ -6,6 +6,7 @@ public class App {
         String message2 = "That was great - lol";
         String target2 = "lol";
 
-        System.out.println(message1.substring(indexOf(v)));
+        System.out.println(message1.replace(target1,""));
+        System.out.println(message2.replace(target2,"laugh out loud"));
     }
 }
