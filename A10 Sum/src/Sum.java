654eaf9f8309683515ec9fc757e20553be992114
Autostrafe <Levibautista360@gmail.com>
2024-04-10 11:25:26 -0600

    A10 Sum Created

---
+++ A10 Sum/src/Sum.java
@@ -0,0 +1,29 @@
+
+public class Sum {
+
+    public static int sum, setNum1, setNum2, a, b;
+
+    public static void setNums(int a, int b){
+
+        setNum1 = a;
+        setNum2 = b;
+
+    }
+    
+    public static void dimSum(int setNum1, int setNum2){
+        
+        int sum = a + b;
+
+    }
+
+    public static void print(int setNum1, int setNum2){
+
+        System.out.println(a +" + " + b + " = " + sum);
+
+    }
+
+
+
+
+
+}
---
+++ A10 Sum/src/SumRunner.java
@@ -0,0 +1,15 @@
+public class SumRunner {
+    public static void main(String[] args) throws Exception {
+
+        Sum.setNums( 10, 20);		
+		Sum.setNums( 20, 10);	
+		Sum.setNums( 20, 20);
+		Sum.setNums( 10, 10);	
+		Sum.setNums( 0, 1);	
+		Sum.setNums( 1, 0);	
+		Sum.setNums( 3, 5);	
+        Sum.setNums( 5, 3 );	
+        Sum.setNums( 5342, 323);	
+        
+    }
+}
