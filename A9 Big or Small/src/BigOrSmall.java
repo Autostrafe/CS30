f615e5ad1481f989db461b992d289440b48ea1a3
Autostrafe <Levibautista360@gmail.com>
2024-04-10 10:41:50 -0600

    Big or Small Finished
    Fixed conditional

--- A9 Big or Small/src/BigOrSmall.java
+++ A9 Big or Small/src/BigOrSmall.java
@@ -3,22 +3,17 @@ import java.util.Scanner;
 
 public class BigOrSmall {
 
-    Scanner scanner = new Scanner(System.in); 
-
-
     public static void check (int a , int b)
     {
-
         if (a > b){
             System.out.println("yes");
         }
-        if (a < b){
+        else if (a < b){
             System.out.println("no");
         }
         else
         {
             System.out.println("equals");
         }
-        Scanner.nextLine();
     }
 }
