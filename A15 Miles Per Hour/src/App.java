4447ab3673c5292cec2342e5f2495f7d24dc52a5
Autostrafe <Levibautista360@gmail.com>
2024-04-19 12:10:02 -0600

    A15 Miles Per Hour Created and Finished

---
+++ A15 Miles Per Hour/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        MilesPerHour.input();
+        MilesPerHour.calculate();
+        MilesPerHour.print();
+    }
+}
---
+++ A15 Miles Per Hour/src/MilesPerHour.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class MilesPerHour{
+    private static double distance, speed, time;
+    private static double minutes, hours, hoursConv;
+ 
+    public static void input(){
+
+         Scanner input = new Scanner(System.in);
+
+         System.out.print("Enter the distance: ");
+
+         double userNum1 = input.nextDouble();
+
+         distance = userNum1;
+
+         System.out.print("Enter the hours: ");
+
+         double userNum2 = input.nextDouble();
+
+         hours = userNum2;
+
+         System.out.print("Enter the minutes: ");
+
+         double userNum3 = input.nextDouble();
+
+         minutes = userNum3;
+    }
+
+    public static void calculate(){
+      hoursConv = minutes/60;
+    
+      time = hours + hoursConv;
+    
+      speed = distance/time;
+
+    }
+
+    public static void print(){
+        System.out.println( (int) Math.floor(distance) + " mile(s) in " + (int) Math.floor(hours) + " hour(s) and " + (int) Math.floor(minutes) + " minute(s) " + (int) Math.floor(speed) + " MPH");
+    }
+
+}
