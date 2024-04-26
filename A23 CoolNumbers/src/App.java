34a89173b55ac8a95fc6e747f65c16d39fec0061
Autostrafe <Levibautista360@gmail.com>
2024-04-26 12:13:30 -0600

    A23 CoolNumbers Started

---
+++ A23 CoolNumbers/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        CoolNumbers.input();
+        CoolNumbers.isCoolNum(250);
+        CoolNumbers.countCoolNumbers(250);
+        CoolNumbers.printCoolNum();
+    }
+}
---
+++ A23 CoolNumbers/src/CoolNumbers.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class CoolNumbers{
+
+    private static int coolNumCount, userNum;
+
+    public static void input(){
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number to check how many cool numbers there are within 6 to you number :: ");
+
+        int userInput = input.nextInt();
+
+        userNum = userInput;
+
+    }
+
+    public static boolean isCoolNum(int i){     
+        if (i % 3 ==1 && i % 4 ==1 && i % 5 ==1 && i % 6 ==1){
+            return true;
+        
+        }
+        else{
+            return false;
+        }
+         
+    }
+
+    public static void countCoolNumbers(int n){
+        coolNumCount = 0;
+        for (int i = 6; i < n; i++){
+            if (isCoolNum(i)){
+                coolNumCount++;
+            }
+         }
+    }
+
+    public static void printCoolNum(){
+        System.out.println(coolNumCount + " cool numbers between 6 - " + userNum);
+
+        // System.out.println(countCoolNumbers(250) + " cool numbers between 6 - 250" + coolNumChoice);
+    }
+
+    public static void executeAll(){
+        input();
+        isCoolNum(userNum);
+        countCoolNumbers(userNum);
+        printCoolNum();
+
+    }
+}
