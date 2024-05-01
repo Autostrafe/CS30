bcce769f3306c120918cabdee7510a4a2344e36a
Autostrafe <Levibautista360@gmail.com>
2024-05-01 12:12:07 -0600

    A24 Loop Stats Created

---
+++ A24 Loop Stats/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your starting number :: ");
+        int startNum = input.nextInt();
+        System.out.println("Enter your ending number :: ");
+        int endNum = input.nextInt();
+
+        
+    }
+}
